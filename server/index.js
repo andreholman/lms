41f9b24662d7db5c5fc5c25576bdc21535706283
Andre Holman <andre@andreholman.com>
2024-01-21 16:14:55 -0800

    connect psql db

--- server/index.js
+++ server/index.js
@@ -1,17 +1,30 @@
 require("dotenv").config({path: __dirname + "/.env"})
 
 const express = require("express");
+const pool = require(__dirname + "/config/db.config.js");
 
 const app = express();
 
 const PORT = process.env.PORT || 9000;
 
-//Here you can add your routes
-//Here's an example
+app.use((req, res, next) => { // middleware
+	console.log(`${req.ip} | ${req.method} ${req.url}`);
+	next();
+});
+
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+	res.send("Hello World!");
+});
+
+app.get("/branches", (req, res) => {
+	pool.query("SELECT * FROM branches", (error, branches) => {
+		if (error) {
+			throw error;
+		}
+		res.status(200).json(branches.rows);
+	});
 });
 
 app.listen(PORT, () => {
-    console.log(`Server listening on the port ${PORT}`);
+	console.log(`Server listening on the port ${PORT}`);
 })
